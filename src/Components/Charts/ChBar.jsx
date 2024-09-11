77510b5c011648dc02b9b662404c3a01012e0f2d
Fatemeh Mn <fatemee.mn24@gmail.com>
2024-09-11 19:20:46 +0330

    editfinal

--- src/Components/Charts/ChBar.jsx
+++ src/Components/Charts/ChBar.jsx
@@ -12,7 +12,7 @@ import Interactionchart3 from './Chart4/Barchart3';
 import Interaction from './Chart1/Barchart';
 export default function ChBar() {
     return (
-        <div className='w-full *:my-[15px] flex flex-wrap justify-evenly items-center'>
+        <div className='w-full *:my-[15px] flex flex-wrap justify-between items-center'>
             <div className='lg:w-[24%] w-full bg-white shadow-lg rounded-lg flex justify-center items-center flex-wrap'>
                 <div className='w-full flex justify-evenly items-center'>
                     <div className='w-[60%] my-[20px] flex justify-evenly items-center'>
@@ -23,7 +23,7 @@ export default function ChBar() {
                         <MoreVertIcon />
                     </span>
                 </div>
-                <div className='w-full flex justify-center md:flex-nowrap flex-wrap lg:justify-evenly items-center'>
+                <div className='w-full flex justify-center md:flex-nowrap flex-wrap lg:justify-between items-center'>
                     <div className='flex lg:w-[60%] w-[60%] justify-start items-center'>
                         <Interaction />
                     </div>
@@ -55,7 +55,7 @@ export default function ChBar() {
                 </div>
             </div>
             {/* ////////////// Box 3 ////////////////////////// */}
-            <div className='lg:w-[24%] w-full bg-white shadow-lg rounded-lg flex justify-center items-center flex-wrap'>
+            <div className='lg:w-[24%] w-full bg-white shadow-lg rounded-lg flex justify-between items-center flex-wrap'>
                 <div className='w-full flex justify-evenly items-center'>
                     <div className='w-[60%] my-[20px] flex justify-evenly items-center'>
                         <span className='flex justify-center items-center px-[6px] py-[5px] bg-green-100 rounded-sm'><AssignmentIcon className='text-[#036844] text-[20px]' /></span>
@@ -66,17 +66,17 @@ export default function ChBar() {
                     </span>
                 </div>
                 <div className='w-full flex justify-center md:flex-nowrap flex-wrap lg:justify-between items-center'>
-                    <div className='flex lg:w-auto w-[60%] justify-start items-center'>
+                    <div className='flex lg:w-[60%] w-[60%] justify-start items-center'>
                         <Interactionchart2 />
                     </div>
-                    <span className='flex w-[35%] justify-start *:w-full items-center flex-wrap'>
+                    <span className='flex w-[35%] justify-center *:w-full items-center flex-wrap'>
                         <p className='font-loginform3 text-[#1d2630]'>1,568</p>
                         <p className='text-[18px] font-text1 text-[#036844]'><CallMadeIcon style={{fontSize:'15px'}} className='text-sm' /> 43.2%</p>
                     </span>
                 </div>
             </div>
             {/* ////////////// Box 4 ////////////////////////// */}
-            <div className='lg:w-[24%] w-full bg-white shadow-lg rounded-lg flex justify-center items-center flex-wrap'>
+            <div className='lg:w-[24%] w-full bg-white shadow-lg rounded-lg flex justify-between items-center flex-wrap'>
                 <div className='w-full flex justify-evenly items-center'>
                     <div className='w-[60%] my-[20px] flex justify-evenly items-center'>
                         <span className='flex justify-center items-center px-[6px] py-[5px] bg-red-100 rounded-sm'><CloudDownloadIcon className='text-[#be3345] text-[20px]' /></span>
@@ -87,10 +87,10 @@ export default function ChBar() {
                     </span>
                 </div>
                 <div className='w-full flex justify-center md:flex-nowrap flex-wrap lg:justify-between items-center'>
-                    <div className='flex lg:w-auto w-[50%] justify-start items-center'>
+                    <div className='flex lg:w-[60%] w-[50%] justify-start items-center'>
                         <Interactionchart3 />
                     </div>
-                    <span className='flex w-[35%] justify-start *:w-full items-center flex-wrap'>
+                    <span className='flex w-[35%] justify-center *:w-full items-center flex-wrap'>
                         <p className='font-loginform3 text-[#1d2630]'>200$</p>
                         <p className='text-[18px] font-text1 text-[#be3345]'><SouthEastIcon style={{fontSize:'15px'}} className='text-sm' /> 21.7%</p>
                     </span>
